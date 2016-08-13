4037c1e67eba509e2ff218894f89df7ad480e5c9
fnk0c <franco.c.colombino@gmail.com>
2016-08-13 14:15:17 -0300

    Create PKGCONFIG, move to its directories and add to DB automaticly

--- gen_pkgconfig.py
+++ gen_pkgconfig.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 #coding=utf-8
 
+import csv
+
 __AUTHOR__	= "Fnkoc"
 
 #This script is part of Organon's project.
@@ -12,44 +14,104 @@ try:
 except:
 	raw_input = input
 
-process = []
+""" ADD DESCRIPTION TO THIS FUNCTION"""
+def pkgconfig():
 
-maintaner = raw_input("Maintaner: ")
-if maintaner == "":
-	maintaner = "Fnkoc"
+	process = []
 
-pkgname = raw_input("Package Name: ")
-arch = raw_input("Arch (x86, x86_64, any): ")
-source = raw_input("Source (https://download.com): ")
+	maintaner = raw_input("Maintaner: ")
+	if maintaner == "":
+		maintaner = "Fnkoc"
 
-version = raw_input("Version: ")
-if version == "":
-	version = "git"
+	pkgname = raw_input("Package Name: ")
+	arch = raw_input("Arch (x86, x86_64, any): ")
+	source = raw_input("Source (https://download.com): ")
 
-print("Process: ")
-for i in iter(raw_input, ":q"):
-	step = raw_input(">>> ")
-	process.append(step)
-installer = raw_input("Installer: ").lower()
-if installer != "none":
-	language = raw_input("Language: ")
+	version = raw_input("Version: ")
+	if version == "":
+		version = "git"
 
-keepgoing = raw_input(" [!] Confirm? [Y/n]").lower()
-if keepgoing == "n":
-	print(" [-] Canceled")
-	exit()
-else:
-	with open("%s.conf" % pkgname, "w") as f:
-		f.write("maintaner = %s\n" % maintaner)
-		f.write("pkgname = %s\n" % pkgname)
-		f.write("version = %s\n" % version)
-		f.write("arch = %s\n" % arch)
-		f.write("source = %s\n" % source)
-		f.write("process = \n{\n")
-		for i in process:
-			f.write("%s\n" % i)
-		f.write("}\n")
-		f.write("installer = %s\n" % installer)
-		if installer != "none":
-			f.write("type = %s" % language)
-		
+	print("Process: ")
+
+	exit = False
+	while exit != True:
+		step = raw_input(">>> ")
+		if step != ":q":
+			process.append(step)
+		else:
+			exit = True
+
+	installer = raw_input("Installer: ").lower()
+	if installer != "none":
+		language = raw_input("Language: ")
+
+	keepgoing = raw_input(" [!] Confirm? [Y/n]").lower()
+	if keepgoing == "n":
+		print(" [-] Canceled")
+		exit()
+	else:
+		with open("%s.conf" % pkgname, "w") as f:
+			f.write("maintaner = %s\n" % maintaner)
+			f.write("pkgname = %s\n" % pkgname)
+			f.write("version = %s\n" % version)
+			f.write("arch = %s\n" % arch)
+			f.write("source = %s\n" % source)
+			f.write("process = \n{\n")
+			for i in process:
+				f.write("%s\n" % i)
+			f.write("}\n")
+			f.write("installer = %s\n" % installer)
+			if installer != "none":
+				f.write("type = %s" % language)
+
+	return (pkgname, version, arch) #add description
+
+def database(pkgname, version, arch):
+	os = ["arch", "debian"]
+	if arch == "any":
+		arch_path = ["x86_64", "i686"]
+	elif arch == "x86":
+		arch_path = ["i686"]
+	elif arch == "x86_64":
+		arch_path = ["x86_64"]
+
+	"""Move pkgconfig to its path"""
+	for i in os:
+		for j in arch_path:
+			full_path = "%s/%s/pkgconfig/" % (i, j)
+			print("cp %s %s" %(pkgname, full_path))
+	print("rm %s" % pkgname)
+
+	"""Update Database in order to add package"""
+	source_name = raw_input("Source name: ")
+	dependencies = raw_input("Dependencies: ")
+	description = raw_input("Description: ")
+	for i in os:
+		for j in arch_path:
+			full_path = "%s/%s/tools.db" % (i, j)
+			
+			pkgs = []
+			with open(full_path, "r") as csvfile:
+				csvcontent = csv.reader(csvfile, delimiter=";")
+
+				for row in csvcontent:
+					pkgs.append(row)
+
+			try:
+				with open(full_path, "w") as csvfile:
+					csvcontent = csv.writer(csvfile, delimiter = ";")
+					for pkg in pkgs:
+						csvcontent.writerow(pkg)
+					csvcontent.writerow([pkgname, version, source_name, \
+										dependencies, description])
+
+			except exception:
+				print("eita")
+
+
+if __name__ == "__main__":
+	data = pkgconfig()
+	pkgname = data[0]
+	version = data[1]
+	arch = data[2]
+	database(pkgname, version, arch)
